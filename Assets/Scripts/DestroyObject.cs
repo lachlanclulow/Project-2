adc68660f9b741fc26771cab4d2896c6eea6a93d
lachlanclulow <lachlanclulow@gmail.com>
2016-10-13 14:34:59 +1100

    chunk

---
+++ Assets/Scripts/DestroyObject.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyObject : MonoBehaviour {
+
+    private int edge = 13;
+
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.z < -edge)
+        {
+            Destroy(this.gameObject);
+        }
+	}
+
+    void OnCollisionEnter(Collision coll)
+    {
+        if (gameObject.name == "Wall")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+}
---
+++ Assets/Scripts/ObjectMotion.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectMotion : MonoBehaviour {
+
+    public float maxSpeed = 3;
+    public float minSpeed = 0.2f;
+    public float maxRotateSpeed = 40;
+    private float speed;
+    private float rotateSpeed;
+
+    void Start()
+    {
+        // PowerUps
+        if (minSpeed == maxSpeed)
+        {
+            speed = minSpeed;
+            rotateSpeed = 0;
+        }
+        // Asteroids
+        else
+        {
+            speed = Random.Range(minSpeed, maxSpeed);
+            rotateSpeed = Random.Range(-maxRotateSpeed, maxRotateSpeed);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(0, 0, -speed * Time.deltaTime);
+        transform.Rotate(new Vector3(0, rotateSpeed, 0) * Time.deltaTime);
+    }
+}
